d295a4e4364f0a121ed1dda27e48badea4a897be
rich <rich@localhost.localdomain>
2016-01-04 16:16:12 -0500

    added logging capabilities

--- src/api/waze_api.py
+++ src/api/waze_api.py
@@ -3,10 +3,14 @@ import requests
 import urllib
 import geopy
 import ast
+import env
+
+# Some of the ideas from this code were taken and udpated from this guy:  https://github.com/dferrante/waze.  Thanks!  A great start!
 
 # An example working waze call
 #https://www.waze.com/RoutingManager/routingRequest?from=x%3A-73.8876574+y%3A40.7664011+bd%3Atrue&to=x%3A-73.7721035+y%3A40.7486434+bd%3Atrue&returnJSON=true&returnGeometries=true&returnInstructions=true&timeout=60000&nPaths=2
 #from: http://stackoverflow.com/questions/19082391/php-json-decode-isnt-working-on-a-text-x-json-string-waze-route-calculation-api
+
 
 class WazeLocationNotFoundException(Exception):
     pass
@@ -14,19 +18,22 @@ class WazeLocationNotFoundException(Exception):
 class Geocode(object):
     '''Returns address, latitude, and longitude of a given address (doesn't need to be fully formed, either!)'''
     def __init__(self, place):
-        
+        '''Uses Google V3.0 Engine to translate any non-fully-formed address to a fully formed address and let/long coordinate'''
         geolocator = geopy.geocoders.GoogleV3()
         address, (latitude,longitude) = geolocator.geocode(place)
 
-        self.address = address
-        self.latitude = latitude
-        self.longitude = longitude
+        #set these global variables so that the program can use class.address, class.latitude, etc. to call the data
+        self.address = address #The fully formed address
+        self.latitude = latitude #The latitude coord
+        self.longitude = longitude #the longitude coord
+
+        
         
 
-class Waze(object):
-    _BASE_URL = "http://waze.com/"
-        
-
+class WazeAPI(object):
+    _BASE_URL_ = "http://waze.com/"
+    
+    
     def __routeData(self, fromLocation, toLocation, maxNumberOfRoutes):
         
         params = "from=x%3A"+str(fromLocation.longitude)+"+y%3A"+str(fromLocation.latitude)+"+bd%3Atrue&"+\
@@ -36,26 +43,28 @@ class Waze(object):
                  "returnInstructions=true&"+\
                  "timeout=60000&"+\
                  "nPaths="+str(maxNumberOfRoutes)
+        #Form a URL call to the waze servers
+        url = WazeAPI._BASE_URL_ + "RoutingManager/routingRequest?" + params
 
-        url = Waze._BASE_URL + "RoutingManager/routingRequest?"+params
-
-        print url
+        env.log.info(url)
         r = requests.get(url)
         json = r.json()
         #with open("temp.txt", "w+") as log:
         #    log.write(r.json())
-        print type(json)
+        
         return json
 
-    def routes(self, source, target, maxNumberOfRoutes = 2):
+    def GetRouteData(self, source, destination, maxNumberOfRoutes = 2):
+        #Geocode the source and destination address to get latitude and longitude data
         fromLocation = Geocode(source)
-        toLocation = Geocode(target)
-        #print fromLocation
+        toLocation = Geocode(destination)
 
-        result = self.__routeData(fromLocation, toLocation, maxNumberOfRoutes)
-        
+        #Retrieve route data from Waze
+        raw_data = self.__routeData(fromLocation, toLocation, maxNumberOfRoutes)
+
+        #Parse out only the important stuff: cross times, distances, and lengths
         routes = []
-        for route in result[u"alternatives"]:
+        for route in raw_data[u"alternatives"]:
             directions = []
             for direction in route[u"response"][u"results"]:
                 directions.append({
@@ -65,19 +74,5 @@ class Waze(object):
                     "length": direction[u"length"],
                 })
             routes.append(directions)
-
-        return routes
-
-if __name__ == "__main__":
-    waze = Waze()
-    print "Hello"
-    
-    routes = waze.routes("1447 southland vista court, atlanta", "1355 Bluegrass Lakes Parkway, 30004", 3)
-
-    for index, route in enumerate(routes):
-        print "Route %d" % index
-        print "====================="
-        print "Total length: %g mi" % ((sum([d["length"] for d in route]) / 1000.0) * 0.621371)
-        print "Total time: %g mins" % (sum([d["crossTime"] for d in route]) / 60.0)
-        print "Total time (w/o real time): %g mins" % (sum([d["crossTimeWithoutRealTime"] for d in route]) / 60.0)
-        print
+        
+        return raw_data, routes
